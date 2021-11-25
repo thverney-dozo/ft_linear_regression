e3b7a6a0d772c956f7c3d2baf0abdf2c48b35f65
Thverney-Dozo <theo7420@hotmail.fr>
2021-11-25 07:14:02 +0100

    formula appliyed and writing thetas on a csv

--- linear_regression.py
+++ linear_regression.py
@@ -1,26 +1,50 @@
 import pandas as pd
 import numpy as np
+import os
+
 
 def get_train_data():
     df = pd.read_csv("data.csv") # datafram (read data in file)
-    y = np.array(df.price.values)
-    y_old = y
-    y = scaling(y, max(y))
-    # df.drop("price", axis=1, inplace=True)
-	X = np.array(df.km.values)
+
+    X = np.array(df['km'].values)
     X_old = X
     X = scaling(X, max(X))
-    print(len(X))
+    X = np.c_[np.ones(X.shape[0]), X]
+
+    y = np.array(df['price'].values)
+    y_old = y
+    y = scaling(y, max(y))
+
     return X, X_old, y, y_old
 
 def scaling(data, scale):
     return data / scale
 
-def	calcul_thetas(X, Y, X_old, Y_old, learning_rate):
-        thetas = np.array([0,0])
+def	linear_regression_thetas(thetas, X, y, learning_rate, m):
+	while 1:
+		old_thetas = thetas
+		thetas = thetas - learning_rate * (1 / m) * (X.T @ ((X @ thetas) - y))
+		if np.array_equal(thetas, old_thetas):
+			break
+	return thetas
+
+def	calcul_thetas(X, X_old, y, y_old, learning_rate):
+	m = float(len(X))
+	thetas = np.array([0,0])
+	thetas = linear_regression_thetas(thetas, X, y, learning_rate, m)
+	thetas[0] = thetas[0] * max(y_old)
+	thetas[1] = thetas[1] * (max(y_old) / max(X_old))
+	return thetas
+
+def	thetas_to_csv(thetas):
+
+	mode = 'r+' if os.path.exists("thetas.csv") else 'w'
+	with open("thetas.csv", mode) as fd:
+		data = "theta0,theta1\n" + str(thetas[0]) + ',' + str(thetas[1]) + '\n'
+		fd.write(data)
 
 if __name__ == "__main__":
-    X, X_old, y, y_old = get_train_data()
-
-    print(len(X_train))
-
+		X, X_old, y, y_old = get_train_data()
+		thetas = calcul_thetas(X, X_old, y, y_old, 1)
+		thetas_to_csv(thetas)
+		print("\033[32;3mThetas are in thetas.csv \033[0m")
