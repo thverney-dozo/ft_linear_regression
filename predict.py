1452106f868b428c85ec76bf5f73a895f6debddc
Thverney-Dozo <theo7420@hotmail.fr>
2021-11-26 06:26:54 +0100

    predict car price and open graph if option --graph

--- predict.py
+++ predict.py
@@ -6,6 +6,7 @@ parser = argparse.ArgumentParser(
 	description="Predict a car's price.")
 parser.add_argument('kms', help="car's mileage")
 parser.add_argument('--graph', '-g', action='store_true', help='show graph')
+parser.add_argument('--costs', '-c', action='store_true', help='show costs')
 options = parser.parse_args()
 
 def predict_price(theta0, theta1, km):
@@ -17,7 +18,8 @@ if __name__ == "__main__":
 		theta0 = thetas.at[0, 'theta0']
 		theta1 = thetas.at[0, 'theta1']
 	except:
-		print("Failed to load thetas.csv.")
+		print("Failed to load thetas.csv. Execute training program (linear_regression.py)")
+		print("\033[0;31mIn case you wonder, 0 + 0 * km = 0\033[0m")
 		exit()
 	if options.graph:
 		try:
@@ -26,10 +28,18 @@ if __name__ == "__main__":
 			Y = data['price'].values
 			line = theta0 + theta1 * X
 		except:
-			print("Failed to load thetas.csv.")
+			print("Failed to load data.csv.")
+			exit(1)
+	if options.costs:
+		try:
+			data_cost = pd.read_csv("costs.csv")
+			iterations = data_cost['iterations'].values
+			costs = data_cost['costs'].values
+		except:
+			print("Failed to load costs.csv.")
 			exit(1)
 	prediction = predict_price(theta0, theta1, int(options.kms))
-	print("The price for a car with {} km is estimated at {}".format(options.kms, round(prediction)))
+	print("The price for a car with {} km is estimated at \033[32;3m{}\033[0m".format(options.kms, round(prediction)))
 	if options.graph:
 		axes = plt.axes()
 		axes = plt.grid()
@@ -39,3 +49,15 @@ if __name__ == "__main__":
 		plt.xlabel('Mileage')
 		plt.ylabel('Price')
 		plt.show()
+	if options.costs:
+		axes = plt.axes()
+		axes = plt.grid()
+		plt.scatter(iterations, costs)
+		plt.title('Cost vs Iterations')
+		plt.xlabel('Number of Iterations')
+		plt.ylabel('Costs')
+		plt.show()
+		# plt.plot(range(iterations),costs)
+		# plt.xlabel('Number of Iterations')
+		# plt.ylabel('Costs')
+		# plt.title('Cost vs Iterations Analysis')
